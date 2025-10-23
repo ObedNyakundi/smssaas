5392b1b95f2c123a18d29692c7c863c26b39277a
Obed Nyakundi <real.obedpaul@gmail.com>
2025-10-23 09:01:25 +0300

    introduced the curricula model and migration

---
+++ app/Models/Curriculum.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Curriculum extends Model
+{
+    protected $table = 'curricula';
+    
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'image',
+    ];
+}
